8ce3d56bcb4cfb8e246fa3dd1eea7fbea7dbcb0b
Richelle Razon <rrazony@yahoo.com>
2017-10-31 15:50:56 -0700

    Added Navigation component

--- app/components/Navigation/Navigation.jsx
+++ app/components/Navigation/Navigation.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { nav, action } from './styles.css';
+import { container, navContainer, link } from './styles.css';
 
 const Navigation = ({ isAuthed }) => (
-  <div>
-    <nav>
+  <div className={container} >
+    <nav className={navContainer} >
       <NavLinks isAuthed={isAuthed} />
       <ActionLinks isAuthed={isAuthed} />
     </nav>
@@ -19,8 +19,8 @@ Navigation.propTypes = {
 const NavLinks = ({ isAuthed }) => (
   isAuthed
     ? (
-      <ul className={nav}>
-        <li><Link to="/">Home</Link></li>
+      <ul>
+        <li><Link className={link} to="/">Home</Link></li>
       </ul>
     )
     : null
@@ -33,15 +33,15 @@ NavLinks.propTypes = {
 const ActionLinks = ({ isAuthed }) => (
   isAuthed
     ? (
-      <ul className={action}>
+      <ul>
         <li>New Duck</li>
-        <li><Link to="/logout">Logout</Link></li>
+        <li><Link className={link} to="/logout">Logout</Link></li>
       </ul>
     )
     : (
       <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/auth">Authenticate</Link></li>
+        <li><Link className={link} to="/home">Home</Link></li>
+        <li><Link className={link} to="/auth">Authenticate</Link></li>
       </ul>
     )
 );
